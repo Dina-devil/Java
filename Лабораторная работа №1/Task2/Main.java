a0f87d2d53f54c2d53a34a066115f9cbee97795a
Dina-devil <151062754+Dina-devil@users.noreply.github.com>
2024-09-22 21:26:32 +0400

    Update Main.java

--- Лабораторная работа №1/Task2/Main.java
+++ Лабораторная работа №1/Task2/Main.java
@@ -1,1 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String url = "https://test.ru/test/1072/page.jsp?intParam=12345&doubleParam=3.14&textParameter=someText";
 
+        SimpleUrl parsedUrl = SimpleUrl.parseUrl(url);
+
+        System.out.println(parsedUrl);
+    }
+}
